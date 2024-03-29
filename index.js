2ae2a2b8c978b8fa272dfa26f86659464baa2e24
Aciku <155045283+Aciku@users.noreply.github.com>
2024-03-29 18:31:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var latestPosts = [
-     // Tambahkan postingan lebih lanjut jika diperlukan ke atas 
-
-        { title: "Kisah Kontroversi: Pejabat Binance Kabur dari Penahanan Nigeria", url: "p/6.html" }, 
-        { title: "Cara Melihat dan Mengelola Aktivitas Google dengan Mudah", url: "p/5.html" }, 
-        { title: "Waspadai Bahaya VPN Gratis di Google Play: Potensi Pembajakan Android", url: "p/4.html" }, 
-        { title: "Apakah WiFi Bisa Melihat Riwayat Pencarian Anda?", url: "p/3.html" }, 
-        { title: "Studi Temukan Penggunaan ChatGPT pada Pelajar Berkaitan dengan Meningkatnya Kesepian ", url: "p/2.html" },
-        { title: "Robert F. Kennedy Jr. Mengumumkan Nicole Shanahan Sebagai Calon Wakil Presiden ", url: "p/1.html" }
-        
-        
-         
+        { title: "Kisah Kontroversi: Pejabat Binance Kabur dari Penahanan Nigeria", url: "6.html" }, 
+        { title: "Cara Melihat dan Mengelola Aktivitas Google dengan Mudah", url: "5.html" }, 
+        { title: "Waspadai Bahaya VPN Gratis di Google Play: Potensi Pembajakan Android", url: "4.html" }, 
+        { title: "Apakah WiFi Bisa Melihat Riwayat Pencarian Anda?", url: "3.html" }, 
+        { title: "Studi Temukan Penggunaan ChatGPT pada Pelajar Berkaitan dengan Meningkatnya Kesepian ", url: "2.html" },
+        { title: "Robert F. Kennedy Jr. Mengumumkan Nicole Shanahan Sebagai Calon Wakil Presiden ", url: "1.html" }
     ];
 
     function generateLatestPostsHTML() {
@@ -23,12 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById("latest-posts-list").innerHTML = generateLatestPostsHTML();
 
+    // Tentukan basePath berdasarkan lokasi
+    var currentLocation = window.location.pathname;
+    var basePath = currentLocation.startsWith("/p/") ? "../p" : " https://aciku.github.io/Berita-Terkini/p";
+
     // Tambahkan event listener untuk setiap elemen <li>
     var listItems = document.querySelectorAll("#latest-posts-list li");
     listItems.forEach(function(item, index) {
         item.addEventListener('click', function() {
-            window.location.href = latestPosts[index].url;
+            var url = basePath + "/" + latestPosts[index].url;
+            window.location.href = url;
         });
     });
 });
-
