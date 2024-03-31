ac8a5d0219750ed4cccaba3dc4fc59b60c056eee
Aciku <155045283+Aciku@users.noreply.github.com>
2024-04-01 00:30:58 +0700

    Create btthn.js

---
+++ donghua/btthn.js
@@ -0,0 +1,88 @@
+    // Tangkap elemen tabs
+
+    var tabsContainer = document.querySelector('.tabs tbody');
+
+    // Array yang berisi link untuk setiap episode
+
+    var episodeLinks = [
+
+        
+        
+
+        ' ',
+
+        ' ',
+
+        ' '
+
+    ];
+
+    // Mendapatkan URL halaman saat ini
+
+    var currentPageUrl = window.location.href.toLowerCase(); // Ubah ke huruf kecil
+
+    // Indeks episode terbaru
+
+    var latestEpisodeIndex = 23; // Misalnya episode terbaru adalah index 23 dalam array
+
+    // Buat tab-episode secara dinamis
+
+    var row = document.createElement('tr');
+
+    var counter = 0;
+
+    episodeLinks.forEach(function(episodeUrl, index) {
+
+        var tab = document.createElement('a');
+
+        tab.setAttribute('href', episodeUrl);
+
+        // Jika URL halaman saat ini terkandung dalam URL tab, atur tab sebagai aktif
+
+        if (currentPageUrl.includes(episodeUrl.toLowerCase())) {
+
+            tab.classList.add('active');
+
+        }
+
+        // Tambahkan teks nomor episode
+
+        var episodeNumber = document.createElement('span');
+
+        episodeNumber.textContent = (index + 1);
+
+        tab.appendChild(episodeNumber);
+
+        // Jika ini adalah episode terbaru, tambahkan label "New" dengan gaya yang ditentukan
+
+        if (index === latestEpisodeIndex) {
+
+            var newLabel = document.createElement('span');
+
+            newLabel.textContent = 'baru';
+
+            newLabel.classList.add('new-label');
+
+            tab.appendChild(newLabel);
+
+        }
+
+        var cell = document.createElement('td');
+
+        cell.appendChild(tab);
+
+        row.appendChild(cell);
+
+        counter++;
+
+        // Setelah setiap 7 tab, buat baris baru
+
+        if (counter % 7 === 0 || index === episodeLinks.length - 1) {
+
+            tabsContainer.appendChild(row);
+
+            row = document.createElement('tr');
+
+        }
+
+    });
