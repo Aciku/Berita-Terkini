50045b89f7c526798aeccc36579db25b006d8370
Aciku <155045283+Aciku@users.noreply.github.com>
2024-04-01 01:56:06 +0700

    Update btth.js

--- donghua/btth.js
+++ donghua/btth.js
@@ -5,7 +5,7 @@ var tabsContainer = document.querySelector('.tabs tbody');
 var episodeLinks = [
     'https://playerprem.blogspot.com/2024/03/btth-s-5-episode-85-sub-indonesia.html?m=1',
     'https://playerprem.blogspot.com/2024/03/btth-s5-episode-86-sub-indonesia.html?m=1',
-    'https://playerprem.blogspot.com/2024/03/blog-po.html?m=1'
+    'https://playerprem.blogspot.com/2024/03/btth-s-5-episode-87-sub-indonesia.html'
     // Tambahkan link episode berikutnya di sini
 ];
 
